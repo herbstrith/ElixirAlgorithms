571b8dc5b9234931fb1f5243103cf823beff0189
Herbstrith <viniherbstrith@gmail.com>
2018-03-14 23:36:23 -0300

    added HashTable struct with a Mod Hash Function (WIP) added some unit tests to HashTable module

---
+++ dataStructures/hashTable.ex
@@ -0,0 +1,36 @@
+defmodule HashTable do
+defstruct table: %{}, hashFunctionSelector: 0, hashFunctionParameter: 10
+ def insert(hashTable, element) do
+   hashFunction = getHashFunction(hashTable)
+   %HashTable{
+     table: Map.update(hashTable.table, hashFunction.(element), [element], &([element|&1])),
+     hashFunctionSelector: hashTable.hashFunctionSelector,
+     hashFunctionParameter: hashTable.hashFunctionParameter
+   }
+ end
+
+ def delete(hashTable, element) do
+   hashFunction = getHashFunction(hashTable)
+   alteredHashEntry = hashTable.table[hashFunction.(element)]|>List.delete(element)
+   %HashTable{
+     table: Map.put(hashTable.table, hashFunction.(element), alteredHashEntry),
+     hashFunctionSelector: hashTable.hashFunctionSelector,
+     hashFunctionParameter: hashTable.hashFunctionParameter
+   }
+ end
+
+ def search(hashTable, element) do
+   hashFunction = getHashFunction(hashTable)
+   Map.get(hashTable.table, hashFunction.(element), []) |> Enum.find(&(&1 == element))
+ end
+
+ defp getHashFunction(hashTable) do
+   case hashTable.hashFunctionSelector do
+     0 -> &modHash(&1, hashTable.hashFunctionParameter)
+   end
+ end
+
+ defp modHash(element, hashMod) do
+   rem(hashMod, element)
+ end
+end
