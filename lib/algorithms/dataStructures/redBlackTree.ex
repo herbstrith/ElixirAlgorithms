f7de6e7c5f4d9b6470864f128124ddc524ead9ff
Herbstrith <viniherbstrith@gmail.com>
2018-04-21 17:39:36 -0300

    working delete implementation for Red black trees added unit tests for Red black tree deletion

--- lib/algorithms/dataStructures/redBlackTree.ex
+++ lib/algorithms/dataStructures/redBlackTree.ex
@@ -3,23 +3,26 @@ defmodule Algorithms.DataStructures.RedBlackTree do
      A red-black tree is approximately balanced
      A red-black tree is a binary tree that satisfies the following red-black properties:
      1. Every node is either red or black.
-     2. The root is black.
+     2. The root is black.   * We relax this rule as per to make deletion simpler
      3. Every leaf (NIL) is black.
      4. If a node is red, then both its children are black.
      5. For each node, all simple paths from the node to descendant leaves contain the
        same number of black nodes.
+
      Using the implementantion of insertion found in the article
      "Red-black trees in a functional setting" by Chris Okasaki
+     Using the deletation implementation found in the article
+     "FUNCTIONAL PEARL Deletion: The curse of the red-black tree" by Kimball Germane and Matthew Might
    """
    alias Algorithms.DataStructures.RedBlackTree, as: RBNode
-   defstruct key: nil, left: nil, right: nil, data: nil, color: :black
+   defstruct key: nil, left: nil, right: nil, data: nil, color: nil
 
    def create() do
      %RBNode{}
    end
 
    # haskel equivalent: balance B (T R (T R a x b) y c) z d = T R (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :black, leftNode = %RBNode{color: :red, left: %RBNode{color: :red}}, x, rightNode) do
+   defp balance(_nodeColor = :black, leftNode = %RBNode{color: :red, left: %RBNode{color: :red}}, x, rightNode) do
      %RBNode{
        color: :red,
        left: %RBNode{
@@ -42,7 +45,7 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    # haskel equivalent: balance B (T R a x (T R b y c)) z d = T R (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :black, leftNode = %RBNode{color: :red, right: %RBNode{color: :red}}, x, rightNode) do
+   defp balance(_nodeColor = :black, leftNode = %RBNode{color: :red, right: %RBNode{color: :red}}, x, rightNode) do
      %RBNode{
        color: :red,
        left: %RBNode{
@@ -65,7 +68,7 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    # haskel equivalent: balance B a x (T R (T R b y c) z d) = T R (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :black, leftNode, x, rightNode = %RBNode{color: :red, left: %RBNode{color: :red}}) do
+   defp balance(_nodeColor = :black, leftNode, x, rightNode = %RBNode{color: :red, left: %RBNode{color: :red}}) do
      %RBNode{
        color: :red,
        left: %RBNode{
@@ -88,7 +91,7 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    #haskel original: balance B a x (T R b y (T R c z d)) = T R (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :black, leftNode, x, rightNode = %RBNode{color: :red, right: %RBNode{color: :red}}) do
+   defp balance(_nodeColor = :black, leftNode, x, rightNode = %RBNode{color: :red, right: %RBNode{color: :red}}) do
      %RBNode{
        color: :red,
        left: %RBNode{
@@ -112,7 +115,7 @@ defmodule Algorithms.DataStructures.RedBlackTree do
 
    # used in delete
    # haskel equivalent: balance BB a x (T R (T R b y c) z d) = T B (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :blackblack, leftNode, x, rightNode = %RBNode{color: :red, right: %RBNode{color: :red}}) do
+   defp balance(_nodeColor = :blackblack, leftNode, x, rightNode = %RBNode{color: :red, right: %RBNode{color: :red}}) do
      %RBNode{
        color: :black,
        left: %RBNode{
@@ -120,45 +123,45 @@ defmodule Algorithms.DataStructures.RedBlackTree do
          key: x.key,
          data: x.data,
          left: leftNode,
-         right: rightNode.left,
+         right: rightNode.left.left,
        },
        key: rightNode.key,
        data: rightNode.data,
        right: %RBNode{
          color: :black,
-         key: rightNode.right.key,
-         data: rightNode.right.data,
-         left: rightNode.right.left,
-         right: rightNode.right.right,
+         key: rightNode.key,
+         data: rightNode.data,
+         left: rightNode.left.right,
+         right: rightNode.right,
        },
      }
    end
 
    # used in delete
    # haskel equivalent: balance BB (T R a x (T R b y c)) z d = T B (T B a x b) y (T B c z d)
-   def balance(_nodeColor = :black, leftNode = %RBNode{color: :red, right: %RBNode{color: :red}}, x, rightNode) do
+   defp balance(_nodeColor = :blackblack, leftNode = %RBNode{color: :red, right: %RBNode{color: :red}}, x, rightNode) do
      %RBNode{
        color: :black,
        left: %RBNode{
          color: :black,
-         key: x.key,
-         data: x.data,
-         left: leftNode,
-         right: rightNode.left,
+         key: leftNode.key,
+         data: leftNode.data,
+         left: leftNode.left,
+         right: leftNode.right.left,
        },
-       key: rightNode.key,
-       data: rightNode.data,
+       key: leftNode.right.key,
+       data: leftNode.right.data,
        right: %RBNode{
          color: :black,
-         key: rightNode.right.key,
-         data: rightNode.right.data,
-         left: rightNode.right.left,
-         right: rightNode.right.right,
+         key: x.key,
+         data: x.data,
+         left: leftNode.right.right,
+         right: rightNode,
        },
      }
    end
 
-   def balance(nodeColor, leftNode, x, rightNode) do
+   defp balance(nodeColor, leftNode, x, rightNode) do
      %RBNode{
        color: nodeColor,
        left: leftNode,
@@ -169,14 +172,26 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    def insert(actualTreeNode = %RBNode{}, newNode = %RBNode{}) do
-     %RBNode{insertRec(actualTreeNode, newNode) | color: :black}
+     blacken(insertRec(actualTreeNode, newNode))
    end
 
-   def insertRec(_actualTreeNode = nil, newNode = %RBNode{}) do
+   defp blacken(node = %RBNode{color: :red, left: %RBNode{color: :red}}) do
+     %{node | color: :black}
+   end
+
+   defp blacken(node = %RBNode{color: :red, right: %RBNode{color: :red}}) do
+     %{node | color: :black}
+   end
+
+   defp blacken(node = %RBNode{}) do
+     node
+   end
+
+   defp insertRec(_actualTreeNode = nil, newNode = %RBNode{}) do
      %{newNode | color: :red}
    end
 
-   def insertRec(actualTreeNode = %RBNode{}, newNode = %RBNode{}) do
+   defp insertRec(actualTreeNode = %RBNode{}, newNode = %RBNode{}) do
      cond do
        actualTreeNode.key > newNode.key ->
          balance(actualTreeNode.color, insertRec(actualTreeNode.left, newNode), actualTreeNode, actualTreeNode.right)
@@ -187,67 +202,75 @@ defmodule Algorithms.DataStructures.RedBlackTree do
      end
    end
 
-   def delete(actualTreeNode = %RBNode{}, deleteNode = %RBNode{}) do
-     #redden
-     %RBNode{deleteRec(actualTreeNode, deleteNode) | color: :red}
+   def delete(actualTreeNode = %RBNode{}, deleteKey) do
+     redden(deleteRec(actualTreeNode, deleteKey))
    end
 
-   def deleteRec(actualTreeNode = %RBNode{}, deleteNode = %RBNode{color: :red, left: nil, right: nil}) do
+   defp redden(node = %RBNode{color: :black, left: %RBNode{color: :black}, right: %RBNode{color: :black}}) do
+     %{node | color: :red}
+   end
+
+   defp redden(node = %RBNode{}) do
+     node
+  end
+
+  defp deleteRec(actualTreeNode = %RBNode{color: :red, left: nil, right: nil}, deleteKey) do
      cond do
-       actualTreeNode.key == deleteNode.key -> nil
-       actualTreeNode.key != deleteNode.key -> deleteNode
+       actualTreeNode.key == deleteKey -> nil
+       actualTreeNode.key != deleteKey -> actualTreeNode
      end
    end
 
-   def deleteRec(actualTreeNode = %RBNode{}, deleteNode = %RBNode{color: :black, left: nil, right: nil}) do
-     cond do
-       actualTreeNode.key == deleteNode.key -> :emptyempty
-       actualTreeNode.key != deleteNode.key -> deleteNode
+   defp deleteRec(actualTreeNode = %RBNode{color: :black, left: nil, right: nil}, deleteKey) do
+    cond do
+       actualTreeNode.key == deleteKey -> :emptyempty
+       actualTreeNode.key != deleteKey -> actualTreeNode
      end
    end
 
-   def deleteRec(actualTreeNode = %RBNode{}, deleteNode = %RBNode{
-     color: :black,
-     left:  %RBNode{
-       color: :red,
-       left: nil,
-       right: nil,
-     } ,
-     right: nil}) do
+   defp deleteRec(actualTreeNode = %RBNode{
+    color: :black,
+    left:  %RBNode{
+      color: :red,
+      left: nil,
+      right: nil,
+    } ,
+    right: nil}, deleteKey) do
      cond do
-       actualTreeNode.key < deleteNode.key ->  %{ actualTreeNode | color: :black, left: deleteRec(actualTreeNode.left, deleteNode)}
-       actualTreeNode.key == deleteNode.key ->  %{ actualTreeNode.left | left: nil, right: nil }
-       actualTreeNode.key > deleteNode.key -> actualTreeNode
+       actualTreeNode.key > deleteKey ->  %{ actualTreeNode | color: :black, left: deleteRec(actualTreeNode.left, deleteKey)}
+       actualTreeNode.key == deleteKey ->  %{ actualTreeNode.left | left: nil, right: nil }
+       actualTreeNode.key < deleteKey -> actualTreeNode
      end
    end
 
-   def deleteRec(actualTreeNode = %RBNode{}, deleteNode = %RBNode{}) do
+   defp deleteRec(actualTreeNode = %RBNode{}, deleteKey) do
      cond do
-       actualTreeNode.key < deleteNode.key ->  rotate(
+       actualTreeNode.key < deleteKey ->  rotate(
          actualTreeNode.color,
-         deleteRec(%{ actualTreeNode.left | color: :red, left: nil, right: nil}, deleteNode),
+         actualTreeNode.left,
          actualTreeNode,
-         nil)
-       actualTreeNode.key == deleteNode.key ->
+         deleteRec(actualTreeNode.right, deleteKey))
+       actualTreeNode.key == deleteKey ->
          {y, b} = min_del(actualTreeNode.right)
          rotate(
            actualTreeNode.color,
            actualTreeNode.left,
            y,
            b)
-       actualTreeNode.key > deleteNode.key -> rotate(
+       actualTreeNode.key > deleteKey -> rotate(
          actualTreeNode.color,
-         actualTreeNode.left,
+         deleteRec(actualTreeNode.left, deleteKey),
          actualTreeNode,
-         deleteRec(actualTreeNode.right, deleteNode))
+         actualTreeNode.right)
      end
    end
 
    # rotate R (T BB a x b) y (T B c z d) = balance B (T R (T B a x b) y c) z d
-   def rotate(_color = :red,
+   defp rotate(_color = :red,
       leftNode = %RBNode{ color: :blackblack},
       actualNode = %RBNode{},
       rightNode = %RBNode{ color: :black}) do
+
      balance(:black,
       %{ actualNode | color: :red, left: %{leftNode | color: :black}, right: rightNode.left },
       rightNode,
@@ -256,10 +279,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    #rotate R EE y (T B c z d) = balance B (T R E y c) z d
-   def rotate(_color = :red,
+   defp rotate(_color = :red,
       _leftNode = :emptyempty,
       actualNode = %RBNode{},
       rightNode = %RBNode{ color: :black}) do
+
      balance(:black,
       %{ actualNode | color: :red, left: nil, right: rightNode.left },
       rightNode,
@@ -268,22 +292,24 @@ defmodule Algorithms.DataStructures.RedBlackTree do
    end
 
    #rotate R (T B a x b) y (T BB c z d) = balance B a x (T R b y (T B c z d))
-   def rotate(_color = :red,
+   defp rotate(_color = :red,
       leftNode = %RBNode{ color: :black},
       actualNode = %RBNode{},
       rightNode = %RBNode{ color: :blackblack}) do
+
      balance(:black,
       leftNode.left,
       leftNode,
-      %{ actualNode | color: :red, left: leftNode.right, right: rightNode }
+      %{ actualNode | color: :red, left: leftNode.right, right: %{rightNode | color: :black} }
       )
    end
 
    #rotate R (T B a x b) y EE = balance B a x (T R b y E)
-   def rotate(_color = :red,
+   defp rotate(_color = :red,
    leftNode = %RBNode{ color: :black},
    actualNode = %RBNode{},
    _rightNode = :emptyempty) do
+
       balance(:black,
       leftNode.left,
       leftNode,
@@ -292,10 +318,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T BB a x b) y (T B c z d) = balance BB (T R (T B a x b) y c) z d
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :blackblack},
    actualNode = %RBNode{},
    rightNode = %RBNode{ color: :black}) do
+
       balance(:blackblack,
       %{ actualNode | color: :red, left: %{leftNode | color: :black }, right: rightNode.left },
       rightNode,
@@ -304,10 +331,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B EE y (T B c z d) = balance BB (T R E y c) z d
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    _leftNode = :emptyempty,
    actualNode = %RBNode{},
    rightNode = %RBNode{ color: :black}) do
+
       balance(:blackblack,
       %{ actualNode | color: :red, left: nil, right: rightNode.left },
       rightNode,
@@ -316,10 +344,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T B a x b) y (T BB c z d) = balance BB a x (T R b y (T B c z d))
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :black},
    actualNode = %RBNode{},
    rightNode = %RBNode{ color: :blackblack}) do
+
       balance(:blackblack,
       leftNode.left,
       leftNode,
@@ -328,10 +357,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T B a x b) y EE = balance BB a x (T R b y E)
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :black},
    actualNode = %RBNode{},
    _rightNode = :emptyempty) do
+
       balance(:blackblack,
       leftNode.left,
       leftNode,
@@ -340,10 +370,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T BB a w b) x (T R (T B c y d) z e) = T B (balance B (T R (T B a w b) x c) y d) z e
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :blackblack},
    actualNode = %RBNode{},
    rightNode = %RBNode{color: :red, left: %RBNode{color: :black}}) do
+
       balance(:black,
       %{actualNode | color: :red, left: %{leftNode | color: :black}, right: rightNode.left.left },
       rightNode,
@@ -352,10 +383,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B EE x (T R (T B c y d) z e) = T B (balance B (T R E x c) y d) z e
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    _leftNode = :emptyempty,
    actualNode = %RBNode{},
    rightNode = %RBNode{color: :red, left: %RBNode{color: :black}}) do
+
       balance(:black,
       %{actualNode | color: :red, left: nil, right: rightNode.left.left },
       rightNode.left,
@@ -364,10 +396,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T R a w (T B b x c)) y (T BB d z e) = T B a w (balance B b x (T R c y (T B d z e)))
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :red, right: %RBNode{color: :black}},
    actualNode = %RBNode{},
    rightNode = %RBNode{color: :blackblack}) do
+
     %{ leftNode |
       color: :black,
       right: balance(:black,
@@ -379,10 +412,11 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate B (T R a w (T B b x c)) y EE = T B a w (balance B b x (T R c y E))
-  def rotate(_color = :black,
+  defp rotate(_color = :black,
    leftNode = %RBNode{ color: :red, right: %RBNode{color: :black}},
    actualNode = %RBNode{},
    _rightNode = :emptyempty) do
+
     %{ leftNode |
       color: :black,
       right: balance(:black,
@@ -394,32 +428,49 @@ defmodule Algorithms.DataStructures.RedBlackTree do
   end
 
   # rotate color axb= T color a x b
-  def rotate(color = :black,
-   leftNode = %RBNode{},
-   actualNode = %RBNode{},
-   rightNode = %RBNode{}) do
+  defp rotate(color, leftNode, actualNode, rightNode) do
     %{ actualNode | color: color, left: leftNode, right: rightNode }
   end
 
   # min_del (T R E x E) = (x,E)
-  def min_del (actualTreeNode = %RBNode{color: :red, right: nil, left: nil}) do
+  defp min_del (actualTreeNode = %RBNode{color: :red, right: nil, left: nil}) do
     {actualTreeNode, nil}
   end
 
   # min_del (T B E x E) = (x,EE)
-  def min_del (actualTreeNode = %RBNode{color: :black, right: nil, left: nil}) do
+  defp min_del (actualTreeNode = %RBNode{color: :black, right: nil, left: nil}) do
     {actualTreeNode, :emptyempty}
   end
 
   # min_del (T B E x (T R E y E)) = (x,T B E y E)
-  def min_del (actualTreeNode = %RBNode{color: :black, left: nil, right: %RBNode{color: :red, left: nil, right: nil}}) do
-    {actualTreeNode, :emptyempty}
+  defp min_del (actualTreeNode = %RBNode{color: :black, left: nil, right: %RBNode{color: :red, left: nil, right: nil}}) do
+    {actualTreeNode, actualTreeNode.right}
   end
 
   # min_del (T c a x b) = let (x',a') = min_del a
   # in (x',rotate c a' x b)
-  def min_del (actualTreeNode = %RBNode{}) do
+  defp min_del (actualTreeNode = %RBNode{}) do
     {xl, al} = min_del(actualTreeNode.left)
-    {xl, rotate(actualTreeNode.color, al, actualTreeNode,actualTreeNode.right)}
+    {xl, rotate(actualTreeNode.color, al, actualTreeNode, actualTreeNode.right)}
+  end
+
+  def search(_treeNode, _key = nil) do
+    nil
+  end
+
+  def search(_treeNode = nil, _key) do
+    nil
+  end
+
+  def search(treeNode = %RBNode{}, key) do
+    if (treeNode.key == key) do
+      treeNode
+    else
+      if (treeNode.key > key) do
+        search(treeNode.left, key)
+      else
+        search(treeNode.right, key)
+      end
+    end
   end
  end
