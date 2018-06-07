fa7a13d477d94d9581316bbb8a28a33ddaad6927
Herbstrith <viniherbstrith@gmail.com>
2018-06-07 17:38:05 -0300

    added the rodCutting algorithm and unit tests

---
+++ test/DP/rodCutting_test.exs
@@ -0,0 +1,30 @@
+defmodule Algorithms.DP.RodCuttingTest do
+  alias Algorithms.DP.RodCutting, as: RodCutting
+
+  use ExUnit.Case, async: true
+
+  test "Should cut a rod of size 8" do
+    assert RodCutting.cutRod({1,5,8,9,10,17,17,20}, 8) == 22
+  end
+
+  test "Should cut a rod of size 5" do
+    assert RodCutting.cutRod({1,5,8,9,10,17,17,20}, 5) == 13
+  end
+
+  test "Should cut a rod of size 4" do
+    assert RodCutting.cutRod({1,5,8,9,10,17,17,20}, 4) == 10
+  end
+
+  test "Should cut a rod of size 1" do
+    assert RodCutting.cutRod({1,5,8,9,10,17,17,20}, 1) == 1
+  end
+
+  test "Should cut a rod of size 0 (return 0)" do
+    assert RodCutting.cutRod({1,5,8,9,10,17,17,20}, 0) == 0
+  end
+
+  test "Should not cut a rod without a price list that matches its length" do
+    assert_raise FunctionClauseError, fn -> RodCutting.cutRod({1,5}, 4) end
+  end
+
+end
