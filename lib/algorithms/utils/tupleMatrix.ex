18ec57c2a0d186cdcc101f297b4858a5e9e1675e
Herbstrith <viniherbstrith@gmail.com>
2018-07-08 22:16:34 -0300

    added Util module and matrixTuple to it renamed lcs test using matrixTuple in lcs

---
+++ lib/algorithms/utils/tupleMatrix.ex
@@ -0,0 +1,13 @@
+defmodule Algorithms.Util.TupleMatrix do
+  def put(tuple_matrix, pos_i, pos_j, value) do
+    line_i = elem(tuple_matrix, pos_i)
+    edited_line_i = put_elem(line_i, pos_j, value)
+    put_elem(tuple_matrix, pos_i, edited_line_i)
+  end
+
+  def get(tuple_matrix, pos_i, pos_j) do
+    line_i = elem(tuple_matrix, pos_i)
+    elem(line_i, pos_j)
+  end
+
+end
---
+++ test/utils/tupleMatrix_test.exs
@@ -0,0 +1,28 @@
+defmodule Algorithms.Util.TupleMatrixTest do
+  alias Algorithms.Util.TupleMatrix, as: TupleMatrix
+
+  use ExUnit.Case, async: true
+
+  test "Should get from a tuple matrix" do
+    test_matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}}
+    assert TupleMatrix.get(test_matrix, 0, 0) == 1
+    assert TupleMatrix.get(test_matrix, 0, 1) == 2
+    assert TupleMatrix.get(test_matrix, 0, 2) == 3
+    assert TupleMatrix.get(test_matrix, 0, 3) == 4
+    assert TupleMatrix.get(test_matrix, 1, 0) == 5
+    assert TupleMatrix.get(test_matrix, 1, 1) == 6
+    assert TupleMatrix.get(test_matrix, 1, 2) == 7
+    assert TupleMatrix.get(test_matrix, 1, 3) == 8
+  end
+
+  test "Should put in a tuple matrix" do
+    test_matrix = {{0, 0, 0}, {1, 2}}
+    test_matrix = TupleMatrix.put(test_matrix, 0, 0, 1)
+    assert TupleMatrix.get(test_matrix, 0, 0) == 1
+    test_matrix = TupleMatrix.put(test_matrix, 0, 1, 2)
+    assert TupleMatrix.get(test_matrix, 0, 1) == 2
+    test_matrix = TupleMatrix.put(test_matrix, 1, 1, 9)
+    assert TupleMatrix.get(test_matrix, 1, 1) == 9
+  end
+
+end
